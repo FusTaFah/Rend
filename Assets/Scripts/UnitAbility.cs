a80390811471a87f3b685b8a2aeceb5560ec8e3a
FusTaFah <mfadel_1990@hotmail.com>
2016-10-28 16:14:07 +0100

    changes unitability

--- Assets/Scripts/UnitAbility.cs
+++ Assets/Scripts/UnitAbility.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class UnitAbility : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    string m_abilityName;
+    float m_cooldown;
+    float m_cooldownTimer;
+
+    public UnitAbility(string abilityName, float cooldown)
+    {
+        m_abilityName = abilityName;
+        m_cooldown = cooldown;
+        m_cooldownTimer = 0.0f;
+    }
+
+    public void UpdateCooldown(float deltaTime)
+    {
+        m_cooldownTimer -= deltaTime;
+    }
+
+    public string GetAbilityName()
+    {
+        return m_abilityName;
+    }
+
+    public float GetCurrentCooldown()
+    {
+        return m_cooldownTimer;
+    }
+
+    public float GetMaxCooldown()
+    {
+        return m_cooldown;
+    }
 }
